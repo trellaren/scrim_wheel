7fa096b98e0cb488519dff67d9a7202f626b2e3f
trellaren <10820376+trellaren@users.noreply.github.com>
2026-04-16 23:56:28 -0600

    Updated for packaging. Doesn't compare the last match lobby with the active lobby But it does run the command line utilities and fill the wheelofnames textbox
    Introduces various commands to the cmd line to run just the cmd line (need to tweak and fix this)

---
+++ src/spin/main.py
@@ -0,0 +1,62 @@
+import random
+from typing import List, Annotated
+import spin.popflash as pf
+import spin.wheel as won
+
+import typer
+from rich.progress import track
+
+app = typer.Typer(help="A popflash helper for rolling out users")
+
+
+@app.command()
+def spin(
+        name: Annotated[str, typer.Argument()] = "fragworks",
+        spins: Annotated[int, typer.Option(help="Number of users to spin")] = 0,
+        wheel: Annotated[bool, typer.Option(help="Use Wheel Of Names Visual Spinner")] = False,
+        drop: Annotated[list[str] | None, typer.Option(help="Drop a specific ")] = None,
+        move: Annotated[bool, typer.Option(help="INACTIVE Moves rolled players from last match to waiting room")] = False
+     #   lastMatch: Annottate[]
+    ):
+    """
+    Extract the names from last popflash match \n
+    optionally with --spins for the number of users to spin \n
+    optionally with --wheel for using a wheelofnames in a browser \n
+    optionally with --drop to force drop specified users from the list.
+    """
+    playersInLobby = pf.getActivePlayers(name)
+    print(playersInLobby)
+    lastMatchLink = pf.getLastMatch(name)
+    print(lastMatchLink)
+    lastMatchPlayerNames = pf.getLastMatchPlayerNames(lastMatchLink)
+    print(lastMatchPlayerNames)
+
+    playersToSpinFrom = lastMatchPlayerNames
+
+    if drop:
+        for d in drop:
+            playersToSpinFrom.pop(d)
+    
+    if wheel:
+        won.populateWheelOfNames(playersToSpinFrom)
+    
+    if spins and not wheel:
+        for spin in spins:
+            spunUser = random.choice(playersToSpinFrom)
+            print(f"Congrats {spunUser}, you have been spun out for the next match")
+        total = 0
+        for value in track(range(100), description="Spinning and idiot..."):
+            #Fake Processing time
+            time.sleep(0.01)
+            total += 1
+        print(f"Processed {total} things.")
+        #TODO Hook this up.
+
+    ## TODO check how many players left from the last scrim
+    ## TODO check 
+    
+# pf.closeBrowser()
+# won.closeBrowser()
+
+if __name__ == "__main__":
+    app()
